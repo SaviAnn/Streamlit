4161df224388ef0dd984cc6309d8261d208cc34f
Anna Savitskaya <126320611+SaviAnn@users.noreply.github.com>
2023-04-28 19:46:07 +0300

    Update LogReg.py

--- LogReg.py
+++ LogReg.py
@@ -30,7 +30,7 @@ class LogReg:
 
         
    def fit(self, X, y):
-     steps=4000
+     steps=6000
      for i in range(steps):
           yhat = self.intercept_ + np.dot(X, self.coef_)
           sigmoid_yhat = sigmoid(yhat)
